77c1c7770451b7efc71fa90bfe3a3da1f26dc77c
AleRivPrado22 <115108514+AleRivPrado22@users.noreply.github.com>
2022-10-12 00:19:42 -0400

    Laboratory 2 of advanced programming.

---
+++ Lab_1_POO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Lab_1_POO;
+/*
+Laboratorio 1 de PA
+*/
+namespace Programa_1_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto vocho1 = new Auto();
+            vocho1.color = "Azul";
+            vocho1.modelo = 2000;
+            vocho1.marca = "Vocho";
+            Console.WriteLine(vocho1.color);
+            Console.WriteLine(vocho1.modelo);
+            Console.WriteLine(vocho1.marca);
+            vocho1.enciende();
+            vocho1.acelera();
+            vocho1.frena();
+
+            Auto chevy = new Auto();
+            chevy.color = "azul";
+            chevy.modelo = 2010;
+            chevy.marca = "Chevrolet";
+            Console.WriteLine(chevy.color);
+            Console.WriteLine(chevy.modelo);
+            Console.WriteLine(chevy.marca);
+            chevy.enciende();
+            chevy.acelera();
+            chevy.frena();
+            Console.Read();
+        }
+    }
+}
---
+++ Lab_1_POO/Prueba.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_POO
+{   
+    public class Auto
+    {
+        public string color = "";
+        public int modelo = 0;
+        public string marca = "";
+        public void enciende()
+        {
+            Console.WriteLine("Se encendio el auto.");
+        }
+        public void acelera()
+        {
+            Console.WriteLine("El auto esta acelerando.");
+        }
+        public void frena()
+        {
+            Console.WriteLine("El auto ha frenado.");
+        }
+    }
+}
